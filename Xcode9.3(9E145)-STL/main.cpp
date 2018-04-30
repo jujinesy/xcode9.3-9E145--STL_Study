24a889acf67d4c06d5a93397b89e96970c279b86
jujinesyMac <jujinesy@gmail.com>
2018-05-01 06:41:26 +0900

    Initial Commit

---
+++ Xcode9.3(9E145)-STL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode9.3(9E145)-STL
+//
+//  Created by das on 01/05/2018.
+//  Copyright © 2018 das. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
